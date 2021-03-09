e94933ca361f1fd57926c67544b3ccbac21e8737
parul <parulsingh310@gmail.com>
2021-03-09 21:56:51 +0000

    first code commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,12 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void AppiumAutomationtest()
+	{
+		System.out.println("Appiumautomationtest is success");
+	}
+}
---
+++ src/test/java/qaclickacademy/RestAssuredAPITest.java
@@ -0,0 +1,14 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RestAssuredAPITest {
+
+
+@Test
+public void ApiAutomationtest()
+{
+	System.out.println("Apiautomationtest is success");
+}
+
+}
